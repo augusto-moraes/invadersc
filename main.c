12e533074bed8f3eea86b2ecec8e5b9449131989
augusto-moraes <augusto-moraes@github.com>
2019-06-30 14:50:09 -0300

    menu, gameover e animacoes

--- main.c
+++ main.c
@@ -26,9 +26,13 @@ struct Tiro{
 GLuint naveTex2d;
 GLuint inimigoTex2d[12];
 GLuint tiroTex2d;
-GLuint morteTex2d[3];
+GLuint morteTex2d[4];
+//Tela de inicio e Game Over
+GLuint gameOverTex2d;
+GLuint menuTex2d;
 
 static void desenhaSprite(float coluna,float linha, GLuint tex);
+static void desenhaTela(GLuint tex);
 static GLuint carregaArqTextura(char *str);
 
 void carregaTexturas(){
@@ -38,7 +42,7 @@ void carregaTexturas(){
         sprintf(str,".//Sprites//inimigo%d.png",i);
         inimigoTex2d[i] = carregaArqTextura(str);
 
-        sprintf(str,".//Sprites//morte%d.png",i-3);
+        sprintf(str,".//Sprites//morte%d.png",i);
         morteTex2d[i] = carregaArqTextura(str);
     }
 
@@ -47,6 +51,12 @@ void carregaTexturas(){
 
     sprintf(str,".//Sprites//tiro.png");
     tiroTex2d = carregaArqTextura(str);
+
+    sprintf(str,".//Sprites//menu.jpg");
+    menuTex2d = carregaArqTextura(str);
+
+    sprintf(str,".//Sprites//gameover.jpg");
+    gameOverTex2d = carregaArqTextura(str);
 }
 
 static GLuint carregaArqTextura(char *str){
@@ -82,13 +92,29 @@ void desenhaSprite(float coluna,float linha, GLuint tex){
     glDisable(GL_TEXTURE_2D);
 }
 
+void desenhaTela(GLuint tex){
+    glColor3f(1.0, 1.0, 1.0);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f,0.0f); glVertex2f(-1, -1);
+    glTexCoord2f(1.0f,0.0f); glVertex2f(1, -1);
+    glTexCoord2f(1.0f,1.0f); glVertex2f(1, 1);
+    glTexCoord2f(0.0f,1.0f); glVertex2f(-1,1);
+    glEnd();
+    glDisable(GL_BLEND);
+    glDisable(GL_TEXTURE_2D);
+}
+
 //Variaveis guia para os inimigos (Posicao e movimento)
 double xi[9] = {-0.525, -0.375, -0.225, -0.075, 0.075, 0.225, 0.375, 0.525, 0.675};
 double y[3] = {0.925, 0.775, 0.625};
 int alienDown=0, alienDir=0;
 
-//Bool referente ao gamemode
-int gamemode = 1, round=0, animacao=0;
+//Bool referente ao gamemode e contador de round
+int gamemode = 3, round=0;
 
 //Limites da tela
 int limDir=8, limEsq=0;
@@ -96,6 +122,7 @@ int limDir=8, limEsq=0;
 //Variaveis referentes ao tiro
 double velocidadeColisao=0, velocidadeTiro=0;
 
+//Declaracao de funcoes
 void IniciaPlayer();
 void DesenhaTela();
 void DesenhaNave();
@@ -169,10 +196,6 @@ int WINAPI WinMain(HINSTANCE hInstance,
     /* enable OpenGL for the window */
     EnableOpenGL(hwnd, &hDC, &hRC);
 
-    IniciaPlayer();
-    tiro.tela=0;
-    nave.pontos=0;
-    nave.vidas=2;
     /* program main loop */
     while (!bQuit)
     {
@@ -238,7 +261,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 case VK_ESCAPE:
                     if(gamemode == 1)
                         gamemode = 2;
-                    else PostQuitMessage(0);
+                    else
+                        PostQuitMessage(0);
                 break;
 
                 case VK_LEFT:
@@ -323,11 +347,13 @@ void DisableOpenGL (HWND hwnd, HDC hDC, HGLRC hRC)
     ReleaseDC(hwnd, hDC);
 }
 
+//Define posicao inicial do player
 void IniciaPlayer(){
     nave.x = 0;
     nave.y = -0.975;
 }
 
+//Reinicia o jogo
 void reset(){
     gamemode = 1;
     for(int j=0;j<3;j++)
@@ -346,9 +372,12 @@ void reset(){
     limEsq=0;
     round=0;
     nave.pontos=0;
+    nave.viva = 1;
+    nave.vidas = 1;
     IniciaPlayer();
 }
 
+//Cria nova wave de aliens
 void replay(){
     DesenhaAliens();
     gamemode = 1;
@@ -370,16 +399,21 @@ void replay(){
     round++;
 }
 
+//Define a tela (Menu, jogo ou gameover)
 void GameMode(){
     int i, k=0;
     for(i=0;i<9;i++){
         if((wave[0][i] == 1 && y[0]-0.05 <= nave.y + 0.2) || (wave[1][i] == 1 && y[1]-0.05 <= nave.y + 0.2) || (wave[2][i] == 1 && y[2]-0.05 <= nave.y + 0.2)){
-            if(!nave.vidas) gamemode = 2;
+            if(!nave.vidas){
+                nave.viva--;
+                //gamemode = 2;
+            }
             else{
+                nave.viva--;
                 nave.vidas--;
                 round--;
-                Sleep(1000);
-                replay();
+                //Sleep(1000);
+                //replay();
             }
         }
         if(!wave[0][i] && !wave[1][i] && !wave[2][i]) k++;
@@ -388,7 +422,7 @@ void GameMode(){
             replay();
         }
     }
-    printf("Pontuacao: %d \t Vidas: %d \t Round: %d\n", nave.pontos, nave.vidas, round);
+    printf("Pontuacao: %d \t Vidas: %d \t Round: %d\n", nave.pontos, nave.vidas, round+1);
 }
 
 void DesenhaTela(){
@@ -396,48 +430,66 @@ void DesenhaTela(){
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
+//Desenha nave e animacao caso esteja morta
 void DesenhaNave(){
-    desenhaSprite(nave.x-0.05, nave.y+0.1, naveTex2d);
+    if(nave.viva == 1){
+        desenhaSprite(nave.x-0.05, nave.y+0.1, naveTex2d);
 
-    if(nave.dir == -1 && nave.x-0.05 > -0.95)
-        nave.x -= 0.01;
-    if(nave.dir == 1 && nave.x+0.05 < 0.95)
-        nave.x += 0.01;
-}
-
-void alienMorre(int j, int i){
-    animacao++;
-    if(animacao < 15)
-      desenhaSprite(xi[i],y[j], morteTex2d[1]);
-    else
-        if(animacao < 30)
-          desenhaSprite(xi[i],y[j], morteTex2d[1]);
+        if(nave.dir == -1 && nave.x-0.05 > -0.95)
+            nave.x -= 0.01;
+        if(nave.dir == 1 && nave.x+0.05 < 0.95)
+            nave.x += 0.01;
+    }
+    else if(!nave.viva){
+        desenhaSprite(nave.x-0.05, nave.y+0.1, morteTex2d[1]);
+        nave.viva--;
+    }
+    else if(nave.viva > -20){
+        desenhaSprite(nave.x-0.05, nave.y+0.1, morteTex2d[2]);
+        nave.viva--;
+    }
+    else if(nave.viva > -40){
+        desenhaSprite(nave.x-0.05, nave.y+0.1, morteTex2d[3]);
+        if(nave.vidas){
+            nave.viva = 1;
+        }
         else
-          desenhaSprite(xi[i],y[j], morteTex2d[2]);
+            gamemode = 2;
+    }
 }
 
+//Desenha os aliens e cria a animacao caso esteja morto
 void DesenhaAliens(){
     int i, j;
     for(j=0;j<3;j++){
         for(i=0;i<9;i++){
-            if(wave[j][i]){
-
+            if(wave[j][i] == 1){
                 desenhaSprite(xi[i]-0.05, y[j]-0.05, inimigoTex2d[3-j]);
-
                 if(tiro.tela && ((tiro.x-0.01 >= xi[i]-0.05 && tiro.x - 0.01 <= xi[i] + 0.05 && tiro.y <= y[j]+0.05 && tiro.y+0.01 >= y[j]-0.05) || (tiro.x+0.01 >= xi[i]-0.05 && tiro.x+0.01 <= xi[i] + 0.05 && tiro.y <= y[j]+0.05 && tiro.y+0.01 >= y[j]-0.05))){
                     tiro.tela = 0;
-                    //alienMorre(j, i);
-                    animacao = 0;
-                    wave[j][i] = 0;
+                    wave[j][i]++;
                     tiro.shoot = 0;
                     velocidadeColisao -= 0.25;
                     nave.pontos+=10;
                 }
             }
+            else if(wave[j][i] >= 2 && wave[j][i] < 8){
+                desenhaSprite(xi[i]-0.05, y[j]-0.05, morteTex2d[1]);
+                wave[j][i]++;
+            }
+            else if(wave[j][i] >= 8 && wave[j][i] < 14){
+                desenhaSprite(xi[i]-0.05, y[j]-0.05, morteTex2d[2]);
+                wave[j][i]++;
+            }
+            else if(wave[j][i] >= 14 && wave[j][i] < 20){
+                desenhaSprite(xi[i]-0.05, y[j]-0.05, morteTex2d[3]);
+                wave[j][i] = 0;
+            }
         }
     }
 }
 
+//Movimenta os aliens
 void Movimenta(){
     int i;
     if(!wave[0][limDir] && !wave[1][limDir] && !wave[2][limDir]) limDir--;
@@ -467,6 +519,7 @@ void Movimenta(){
     }
 }
 
+//Desenha o tiro
 void DesenhaTiro(){
     if(tiro.shoot && !tiro.tela){
         tiro.tela = 1;
@@ -498,14 +551,18 @@ void DesenhaTiro(){
     }
 }
 
+//Tela de Game over
 void GameOver(){
-    glClearColor(1.0, 0.0, 0.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    desenhaTela(gameOverTex2d);
     //printf("GAME OVER\nPontuacao: %d\nPressione ESPACO para jogar novaente\nPressione Esc para ir para o menu\n", nave.pontos);
 }
 
+//Tela de Menu
 void Menu(){
-    glClearColor(0.0, 0.0, 1.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    desenhaTela(menuTex2d);
     //printf("Space Invaders\nPressione ESPACO para iniciar o jogo\nPressione Esc para sair\n");
 }
